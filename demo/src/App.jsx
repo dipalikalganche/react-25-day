049e2f365569d25767718fc0bdc2e7433edd4866
dipalikalganche <dipalikalganche@gmail.com>
2026-01-28 20:01:38 +0530

    updated day1

---
+++ demo/src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="title">🎉 Day 1 Completed Successfully</h1>
+
+      <ul className="topic-list">
+        <li>
+          <span>Import & Export</span> – Used to share code between files
+        </li>
+        <li>
+          <span>Bundler</span> – Combines multiple files into one
+        </li>
+        <li>
+          <span>What is React</span> – Library to build UI using components
+        </li>
+        <li>
+          <span>Folder Structure</span> – Organizes project files logically
+        </li>
+        <li>
+          <span>Component & Element</span> – Reusable UI & UI description
+        </li>
+        <li>
+          <span>NPM & NPX</span> – Package manager & package runner
+        </li>
+        <li>
+          <span>JSX</span> – HTML-like syntax in JavaScript
+        </li>
+        <li>
+          <span>Props</span> – Data passed from parent to child
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
